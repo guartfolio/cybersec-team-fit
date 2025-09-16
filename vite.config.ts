c17788e6ddb9bfb98c7222fb3da7fd128460badf
FSW_Labs <FSW_Labs@LE-K101-ST-4VXN.FSW.EDU>
2025-09-16 16:28:41 -0400

    Initial deploy

---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+export default defineConfig({
+  // if your repo is named something else, change this path
+  base: '/cybersec-team-fit/',
+  plugins: [react()],
+  resolve: { /* your alias stuff here */ }
+})
