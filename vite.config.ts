4a78da0c055326765127cd7960855fdf3c42642a
guartfolio <brockster19@gmail.com>
2025-09-16 17:06:57 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,10 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  // 👇 must match your repo name EXACTLY
+  // MUST match your repo name exactly:
   base: '/cybersec-team-fit/',
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
+  }
 })
